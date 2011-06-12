4fceab9627b7230441c4271564d6901d9c0ffc9e
Rory McKinley <rorymckinley@gmail.com>
2011-06-12 09:58:51 +0200

    Slight refactor

--- lib/vail/generator.rb
+++ lib/vail/generator.rb
@@ -5,18 +5,17 @@ module Vail
       
       @config = config.merge (
         {
-        Dot => { "duration" => config["dot"]["duration"], "pause" => config["dot"]["pause"]},
-        Dash => { "duration" => config["dash"]["duration"], "pause" => config["dash"]["pause"]}
-      }
+          Dot => { "duration" => config["dot"]["duration"], "pause" => config["dot"]["pause"]},
+          Dash => { "duration" => config["dash"]["duration"], "pause" => config["dash"]["pause"]}
+        }
       )
-
       @config["repetitions"] ||= 1
     end
     def to_morse(phrase)
-      instruction_sets = build_instructions(phrase)
+      instructions = build_instructions(phrase)
 
-      (@config["repetitions"].times.inject([]) { |r,i| r << instruction_sets }).each do |instructions|
-        instructions.each do |i|
+      (@config["repetitions"].times.inject([]) { |r,i| r << instructions }).each do |instruction_set|
+        instruction_set.each do |i|
           execute_instruction(i)
         end
       end
@@ -32,7 +31,10 @@ module Vail
           instructions << { :command => :sleep, :instruction => @config["group"]["pause"].to_f/1000.0}
         else
           morse = Translate.to_morse(char)
-          instructions << { :command => :sound, :instruction => morse.map { |dotdash| { :duration => @config[dotdash]["duration"], :pause => @config[dotdash]["pause"], :frequency => @config["frequency"] } }}
+          instructions << { 
+            :command => :sound, 
+            :instruction => morse.map { |dotdash| { :duration => @config[dotdash]["duration"], :pause => @config[dotdash]["pause"], :frequency => @config["frequency"] }}
+          }
           instructions << { :command => :sleep, :instruction => @config["letter"]["pause"].to_f/1000.0}
         end
       end
